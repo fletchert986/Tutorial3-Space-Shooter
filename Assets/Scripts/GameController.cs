672ee9674dd664b4ad0ae280fce9c2e75c5fd4d9
fletchert986 <42794464+fletchert986@users.noreply.github.com>
2019-04-23 23:59:14 -0400

    forced changes for final

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -20,6 +20,13 @@ public class GameController : MonoBehaviour {
     private bool restart;
     private int score;
 
+    public AudioSource game;
+    /*public AudioClip clip1;
+    public AudioClip clip2;
+    public AudioClip clip3;*/
+    public AudioSource win;
+    public AudioSource lose;
+
     private void Start()
     {
         gameOver = false;
@@ -29,6 +36,11 @@ public class GameController : MonoBehaviour {
         score = 0;
         UpdateScore();
         StartCoroutine (SpawnWaves());
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        //source = audioSources[0];
+        /*clip1 = audioSources[0].clip;
+        clip2 = audioSources[1].clip;
+        clip3 = audioSources[2].clip;*/
     }
 
     private void Update()
@@ -49,15 +61,34 @@ public class GameController : MonoBehaviour {
     {
         yield return new WaitForSeconds(startWait);
         while (true) {
-            for (int i = 0; i < hazardCount; i++)
-            {
-                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
-                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-                Quaternion spawnRotation = Quaternion.identity;
-                Instantiate(hazard, spawnPosition, spawnRotation);
-                yield return new WaitForSeconds(spawnWait);
+            while (score<200) {
+                while (score < 100)
+                {
+                    GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                    Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                    Quaternion spawnRotation = Quaternion.identity;
+                    Instantiate(hazard, spawnPosition, spawnRotation);
+                    yield return new WaitForSeconds(spawnWait);
+                    if (gameOver = true) break;
+                }
+
+                if (gameOver = true) break;
+                yield return new WaitForSeconds(waveWait);
+
+                while (score > 100 && score < 200)
+                {
+                    GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                    Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                    Quaternion spawnRotation = Quaternion.identity;
+                    Instantiate(hazard, spawnPosition, spawnRotation);
+                    yield return new WaitForSeconds(spawnWait);
+                    if (gameOver = true) break;
+                }
+
+                if (gameOver = true) break;
+                yield return new WaitForSeconds(waveWait);
             }
-            yield return new WaitForSeconds(waveWait);
+
             if (gameOver)
             {
                 restartText.text = "Press 'F' for Restart";
@@ -71,7 +102,7 @@ public class GameController : MonoBehaviour {
     {
         score += newScoreValue;
         UpdateScore();
-        if (score >= 100)
+        if (score >= 200)
         {
             Win();
         }
